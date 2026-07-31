2c7eccde400dd55b93ab4da8fb6c8a92b0f4cc50
Yusuf Shaikh | Developer <bhaiyusuf009@gmail.com>
2026-08-01 03:43:09 +0530

    Update supabase.ts

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -7,5 +7,5 @@ export const supabase = createClient(
 
 export const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_KEY!
+  process.env.SUPABASE_SERVICE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
